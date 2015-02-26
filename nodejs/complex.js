64f2be6c18cde1d6b4ef5916c80916ca09baeca9
Bo Cong Anh <nguyenngocphuckt@yahoo.com>
2015-02-26 16:00:48 +0700

    math complex

---
+++ nodejs/complex.js
@@ -0,0 +1,41 @@
+var log=console.log;
+
+function Complex (r,i) {
+	
+  var newObject = Complex.prototype.isPrototypeOf(this)
+                  ? this
+                  : new Complex();
+  newObject.r = r||0;
+  newObject.i = i||0;
+  return newObject;
+}
+
+
+Complex.prototype={
+	toString : function () {
+		if(this.i>0) {
+			return this.r+"+"+this.i+"i";
+		}else if(this.i<0){
+			return this.r+""+this.i+"i";
+		}
+		
+		return this.r;
+	},
+	add:function(o) {
+		return Complex(this.r+o.r,this.i+o.i);
+	},
+	sub:function (o) {
+		return Complex(this.r-o.r,this.i-o.i);
+	},
+	mul:function (o) {
+		return Complex(this.r*o.r-this.i*o.i,this.r*o.i+this.i*o.r);
+	},
+	conj:function(){
+		return Complex(this.r,-this.i);
+	},
+};
+var a= Complex(1,2);
+
+var b=Complex(3,5);
+
+log(a.conj().toString());
